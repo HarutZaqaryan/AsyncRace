57df0f55cb798a6c9e6a99a44f8256f0be52a5b1
Harut <zaqaryanharut242gmail.com>
2024-04-19 23:57:34 +0400

    Creation: Creating a UI without styling

---
+++ src/app/Components/Pages/winners/winners.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  bestTime: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', bestTime: 10 },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', bestTime: 10 },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', bestTime: 10 },
+  {
+    position: 4,
+    name: 'Beryllium',
+    weight: 9.0122,
+    symbol: 'Be',
+    bestTime: 10,
+  },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B', bestTime: 10 },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', bestTime: 10 },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', bestTime: 10 },
+];
+
+@Component({
+  selector: 'app-winners',
+  standalone: true,
+  imports: [MatTableModule],
+  templateUrl: './winners.component.html',
+  styleUrl: './winners.component.scss',
+})
+export class WinnersComponent {
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'weight',
+    'symbol',
+    'bestTime',
+  ];
+  dataSource = ELEMENT_DATA;
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { GarageComponent } from './Components/Pages/garage/garage.component';
+import { NotFoundPageComponent } from './Components/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: GarageComponent,
+  },
+  {
+    path: 'garage',
+    loadComponent: () =>
+      import('./Components/Pages/garage/garage.component').then(
+        (mod) => mod.GarageComponent
+      ),
+  },
+  {
+    path: 'winners',
+    loadComponent: () =>
+      import('./Components/Pages/winners/winners.component').then(
+        (mod) => mod.WinnersComponent
+      ),
+  },
+  {
+    path: '404',
+    component: NotFoundPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+  },
+];
