aa2c8d170e2fdc7a09407597f70ac72f60a5a1e7
Harut <zaqaryanharut242gmail.com>
2024-05-08 23:39:24 +0400

    Styles: Add header styles

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,21 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router,RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  public activeRoute: string = 'garage';
 
-  constructor(private router:Router){}
+  constructor(private router: Router) {}
 
   garageBtnClick() {
     this.router.navigate(['/', 'garage']);
+    this.activeRoute = 'garage';
   }
   winnersBtnClick() {
     this.router.navigate(['/', 'winners']);
+    this.activeRoute = 'winners';
   }
 }
