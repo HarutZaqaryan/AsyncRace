aae561be64fb4de4747baa694c3b11d24206f7c3
Harut <zaqaryanharut242gmail.com>
2024-05-14 21:04:52 +0400

    deploy: Netlify settings

---
+++ dist/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-R43HHFBC.mjs";import"./chunk-IWJIRDU3.mjs";import"./chunk-4J4TRLDD.mjs";import"./chunk-H45YYTUF.mjs";import"./chunk-GTSSCL6B.mjs";import"./chunk-BJMYGEKF.mjs";import"./chunk-KD3K3DA2.mjs";import"./chunk-3LK6IVFU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
